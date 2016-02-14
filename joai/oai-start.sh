e5b8392949c7ceeba250a3a8835cb44442d44efd
Piyapong Charoenwattana <piyapong.charoenwattana@ualberta.ca>
2016-02-13 18:18:09 -0700

    Added joai server.

---
+++ joai/oai-start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -jar jetty-runner.jar --port 9090 --stop-port 9093 --stop-key oaistop oai.war
+
---
+++ joai/oai-stop.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -DSTOP.PORT=9093 -DSTOP.KEY=oaistop -jar start.jar --stop
+
