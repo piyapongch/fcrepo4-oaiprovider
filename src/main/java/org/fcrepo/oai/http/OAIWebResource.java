6b9021d087ac28a3106e32fc727af4de1a1be209
Piyapong Charoenwattana <piyapong.charoenwattana@ualberta.ca>
2016-02-21 10:49:34 -0700

    Before add post method.

--- src/main/java/org/fcrepo/oai/http/OAIWebResource.java
+++ src/main/java/org/fcrepo/oai/http/OAIWebResource.java
@@ -92,7 +92,6 @@ public class OAIWebResource {
      * @throws RepositoryException the repository exception
      */
     @GET
-    @POST
     @Produces(MediaType.TEXT_XML)
     public Object getOAIResponse(final @QueryParam("verb") String verbParam,
         final @QueryParam("identifier") String identifierParam,
